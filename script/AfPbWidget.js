6bb7a016efbec6950f120ea7722ccc75a7102587
Mats Åström <mats.astrom@arbetsformedlingen.se>
2017-06-27 17:19:44 +0200

    Changed css

--- script/AfPbWidget.js
+++ script/AfPbWidget.js
@@ -53,28 +53,6 @@
         var  $afJobCount = $('#afJobCount');
 
 
-        // //Used to load script dependencies
-        // function loadScripts(array, callback) {
-        //     var loader = function (src, handler) {
-        //         var script = document.createElement("script");
-        //         script.src = src;
-        //         script.onload = script.onreadystatechange = function () {
-        //             script.onreadystatechange = script.onload = null;
-        //             handler();
-        //         };
-        //         var head = document.getElementsByTagName("head")[0];
-        //         (head || document.body).appendChild(script);
-        //     };
-        //     (function run() {
-        //         if (array.length != 0) {
-        //             loader(array.shift(), run);
-        //         } else {
-        //             callback && callback();
-        //         }
-        //     })();
-        // }
-
-
         if ($afJobCount.length) {
             if($afJobCount[0].dataset.lanid) {
                 httpRequestString = baseUrl + "matchning?lanid=" + $afJobCount[0].dataset.lanid + "&kommunid=" + $afJobCount[0].dataset.kommunid + "&sida=0&antalrader=1";
